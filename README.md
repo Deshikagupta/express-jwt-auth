539fc195c40545454df0341b1400739d6dc45fec
Deshika Gupta <guptadeshika31@gmail.com>
2025-11-04 18:23:39 +0530

    Code files for auth login-signup

---
+++ README.md
@@ -0,0 +1,8 @@
+# Login-Signup App (Node.js + Express + MongoDB)
+Simple authentication system using bcrypt for password hashing and JWT for tokens.
+
+## Setup
+1. Clone the repo
+2. Run `npm install`
+3. Create `.env` file with `JWT_SECRET=your_secret`
+4. Run `node app.js`
---
+++ app.js
@@ -0,0 +1,67 @@
+const express=require('express');
+const app=express();
+
+const bcrypt=require('bcrypt');
+const path=require('path');
+const cookieParser=require('cookie-parser');
+const userModel=require('./models/user');
+const jwt=require("jsonwebtoken");
+
+app.set('view engine','ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
+app.use(cookieParser());
+require('dotenv').config();
+
+app.get('/',(req,res)=>{
+    res.render("index");
+})
+
+app.post('/create', (req,res)=>{
+    let {username,email,age,password}=req.body;
+
+    bcrypt.genSalt(10,(err,salt)=>{
+        bcrypt.hash(password,salt,async (err,hash)=>{
+            let createdUser = await userModel.create({
+                username,
+                email,
+                password:hash,
+                age
+            })
+
+            let token = jwt.sign({email},process.env.JWT_SECRET);
+            res.cookie("token",token);
+
+            res.send(createdUser);
+        })
+    })
+})
+
+app.get('/login',(req,res)=>{
+    res.render('login');
+})
+
+app.post('/login',async (req,res)=>{
+    console.log(req.body);
+    
+    let user=await userModel.findOne({email: req.body.email});
+    if(!user) return res.send("Something went wrong");
+
+    bcrypt.compare(req.body.password, user.password,function(err,result){
+        if(result){
+            let token = jwt.sign({email: user.email},process.env.JWT_SECRET);
+            res.cookie("token",token);
+            res.send("Ready to Login")
+        }
+        else{
+            res.send("Something went wrong");
+        }
+    })
+})
+
+app.get('/logout',(req,res)=>{
+    res.cookie("token","");
+    res.redirect('/');
+})
+app.listen(3000);
